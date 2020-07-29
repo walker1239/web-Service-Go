db8d238485adc67497a54bf484a3d00a67170a87
Walker Manrique <34077779+walker1239@users.noreply.github.com>
2020-07-29 08:34:56 -0500

    Notas terminadas

--- main.go
+++ main.go
@@ -3,32 +3,36 @@ package main
 import (
 	"log"
 	"net/http"
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
-	"golang.org/x/crypto/bcrypt"
+	"database/sql"
 	"fmt"
 	"encoding/json"
 	"io/ioutil"
-	"strconv"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Note struct{
 	Id int				`json:"id"`
 	Title string		`json:"title"`	
 	Description string	`json:"description"`
-	EmailUser string 		
 }
 
-type User struct{
-	Email string		`json:"email"`		
-	Password string		`json:"password"`	
-	Token string			
+
+func GetConnection() *sql.DB {
+	database, errordb := sql.Open("sqlite3", "./database.db")
+	if errordb != nil {
+		//return []Note{}, err
+		log.Println(errordb.Error())
+	}
+	
+	statement, errorstmt := database.Prepare("CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(100) NULL,description VARCHAR(500) NULL);")
+	if errorstmt != nil {
+		//return []Note{}, err
+		log.Println(errorstmt.Error())
+	}
+	statement.Exec()
+	return database
 }
-
-var notes []Note
-var users []User
-
-var jwtKey = []byte("13Akfq195g")
 
 func holaName(w http.ResponseWriter, r *http.Request){
 	name := mux.Vars(r)["name"]
@@ -36,232 +40,104 @@ func holaName(w http.ResponseWriter, r *http.Request){
 }
 
 func getNotes(w http.ResponseWriter, r *http.Request){
+	db := GetConnection()
+	var notes []Note
+	rows, _ := db.Query("SELECT id, title, description FROM notes")
+    var note Note
+    for rows.Next() {
+		rows.Scan(&note.Id, &note.Title, &note.Description)
+		notes=append(notes,note)
+    }
 	json.NewEncoder(w).Encode(notes)
 }
 
-func getUsers(w http.ResponseWriter, r *http.Request){
-	json.NewEncoder(w).Encode(users)
-}
-
 func getNote(w http.ResponseWriter, r *http.Request){
+	db := GetConnection()
+
 	id_String := mux.Vars(r)["id"]
-	id,_ := strconv.Atoi(id_String)
-	for i := range notes{
-		if notes[i].Id == id{
-			json.NewEncoder(w).Encode(notes[i])
-		}
-	}
-	
-}
-
-func getNotesByUser(w http.ResponseWriter, r *http.Request){
-	var notesUser []Note
-
-	tokenHeader := r.Header.Get("Authorization")
-	for i := range users{
-		if users[i].Token == tokenHeader{
-			for j := range notes{
-				if notes[j].EmailUser == users[i].Email {
-					notesUser=append(notesUser,notes[j])
-				}
-			}
-			json.NewEncoder(w).Encode(notesUser)
-			return
-		}
-	}
-	message1 := map[string]string{
-		"code":"error",
-		"message":"Token no reconocido, vuelva a iniciar sesión",
-	}
-	json.NewEncoder(w).Encode(message1)
+	rows, _ := db.Query("SELECT id, title, description FROM notes WHERE id = "+id_String)
+    var note Note
+    for rows.Next() {
+		rows.Scan(&note.Id, &note.Title, &note.Description)
+    }
+	json.NewEncoder(w).Encode(note)	
 }
 
 func createNote(w http.ResponseWriter, r *http.Request){
+	db := GetConnection()
 	var temp Note
-	tokenHeader := r.Header.Get("Authorization")
 	body,_:=ioutil.ReadAll(r.Body)
 	json.Unmarshal(body,&temp)
-	for i := range users{
-		if users[i].Token == tokenHeader{
-			temp.EmailUser=users[i].Email
-			temp.Id=len(notes)+1
-			notes = append(notes,temp)
-			message1 := map[string]string{
-				"code":"correcto",
-				"message":"Nota registrada correctamente",
-			}
-			json.NewEncoder(w).Encode(message1)
-			return
+	statement, err := db.Prepare("INSERT INTO notes (title, description) VALUES (?, ?)")
+	statement.Exec(temp.Title, temp.Description)
+	if err != nil {
+		message1 := map[string]string{
+			"code":"error",
+			"message":err.Error(),
 		}
+		json.NewEncoder(w).Encode(message1)
+		return
 	}
 	message1 := map[string]string{
-		"code":"error",
-		"message":"Token no reconocido, vuelva a iniciar sesión",
+		"code":"correcto",
+		"message":"Nota registrada correctamente",
 	}
 	json.NewEncoder(w).Encode(message1)
 }
 
 func updateNote(w http.ResponseWriter, r *http.Request){
-	var updateNote Note
-
+	db := GetConnection()
+	var temp Note
 	id_String := mux.Vars(r)["id"]
-	id,_ := strconv.Atoi(id_String)
-
-	tokenHeader := r.Header.Get("Authorization")
 	body,_:=ioutil.ReadAll(r.Body)
-	json.Unmarshal(body,&updateNote)
-	for i := range users{
-		if users[i].Token == tokenHeader{
-			for j := range notes{
-				if notes[j].Id == id && notes[j].EmailUser == users[i].Email {
-					notes[j].Title=updateNote.Title
-					notes[j].Description=updateNote.Description
-					message1 := map[string]string{
-						"code":"correcto",
-						"message":"Nota actualizada correctamente",
-					}
-					json.NewEncoder(w).Encode(message1)
-					return
-				}
-			}
-			message1 := map[string]string{
-				"code":"error",
-				"message":"Nota no encontrada",
-			}
-			json.NewEncoder(w).Encode(message1)
-			return
+	json.Unmarshal(body,&temp)
+	statement, err := db.Prepare("UPDATE notes SET title = ?, description= ? WHERE id = ?")
+	statement.Exec(temp.Title, temp.Description, id_String)
+	if err != nil {
+		message1 := map[string]string{
+			"code":"error",
+			"message":err.Error(),
 		}
+		json.NewEncoder(w).Encode(message1)
+		return
 	}
 	message1 := map[string]string{
-		"code":"error",
-		"message":"Token no reconocido, vuelva a iniciar sesión",
+		"code":"correcto",
+		"message":"Nota actualizada correctamente",
 	}
 	json.NewEncoder(w).Encode(message1)
-}
-
-func remove(notesdel []Note, i int) []Note {
-    notesdel[len(notesdel)-1], notesdel[i] = notesdel[i], notesdel[len(notesdel)-1]
-    return notesdel[:len(notesdel)-1]
 }
 
 func deleteNote(w http.ResponseWriter, r *http.Request){
+	db := GetConnection()
+
 	id_String := mux.Vars(r)["id"]
-	id,_ := strconv.Atoi(id_String)
 
-	tokenHeader := r.Header.Get("Authorization")
-
-	for i := range users{
-		if users[i].Token == tokenHeader{
-			for j := range notes{
-				if notes[j].Id == id && notes[j].EmailUser == users[i].Email {
-					notes=remove(notes,j);
-					message1 := map[string]string{
-						"code":"correcto",
-						"message":"Nota eliminada correctamente",
-					}
-					json.NewEncoder(w).Encode(message1)
-				}
-			}
-			message1 := map[string]string{
-				"code":"error",
-				"message":"Nota no encontrada",
-			}
-			json.NewEncoder(w).Encode(message1)
-			return
-		}
-	}
-	message1 := map[string]string{
-		"code":"error",
-		"message":"Token no reconocido, vuelva a iniciar sesión",
-	}
-	json.NewEncoder(w).Encode(message1)
-}
-
-func createUser(w http.ResponseWriter, r *http.Request){
-	var account User
-	body,_:=ioutil.ReadAll(r.Body)
-	err:=json.Unmarshal(body,&account)
+	statement, err := db.Prepare("DELETE FROM notes WHERE id = ?")
+	statement.Exec(id_String)
 	if err != nil {
-		log.Println(err.Error())
-	}
-	for i := range users{	
-		if users[i].Email == account.Email{
-			message1 := map[string]string{
-				"code":"error",
-				"message":"Correo ya existente",
-			}
-			json.NewEncoder(w).Encode(message1)
-			return
+		message1 := map[string]string{
+			"code":"error",
+			"message":err.Error(),
 		}
+		json.NewEncoder(w).Encode(message1)
+		return
 	}
-	account.Token="none"
-	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(account.Password), bcrypt.DefaultCost)
-	account.Password = string(hashedPassword)
-	users = append(users,account)
 	message1 := map[string]string{
 		"code":"correcto",
-		"message":"Usuario registrado con exito",
-	}
-	json.NewEncoder(w).Encode(message1)
-}
-
-func loginUser(w http.ResponseWriter, r *http.Request){
-	var account User
-
-	body,_:=ioutil.ReadAll(r.Body)
-	json.Unmarshal(body,&account)
-	//notes = append(notes,temp)
-	for i := range users{
-		
-		if users[i].Email == account.Email{
-			err := bcrypt.CompareHashAndPassword([]byte(users[i].Password), []byte(account.Password))
-			if err != nil && err == bcrypt.ErrMismatchedHashAndPassword { 
-				message := map[string]string{
-					"code":"error",
-					"message":"Contraseña o correo invalidos",
-				}
-				json.NewEncoder(w).Encode(message)
-				return
-			}
-			userClaims := jwt.MapClaims{}
-			userClaims["email"] = users[i].Email
-			userClaims["password"] = users[i].Password
-			at := jwt.NewWithClaims(jwt.SigningMethodHS256, userClaims)
-  			token, err := at.SignedString(jwtKey)
-			users[i].Token = token
-			message := map[string]string{
-				"code":"correcto",
-				"message":"Login correcto",
-				"Api": users[i].Token,
-			}
-			json.NewEncoder(w).Encode(message)
-			return
-		}
-	}
-	message1 := map[string]string{
-		"code":"error",
-		"message":"Ocurrio un error",
+		"message":"Nota eliminada correctamente",
 	}
 	json.NewEncoder(w).Encode(message1)
 }
 
 func main(){
 
-	notes=append(notes,Note{Id:1,Title:"titulo1",Description:"description1"})
-	notes=append(notes,Note{Id:2,Title:"titulo2",Description:"description2"})
-
 	r := mux.NewRouter()
 	r.HandleFunc("/note", createNote).Methods("POST")
 	r.HandleFunc("/notes", getNotes).Methods("GET")
-	r.HandleFunc("/user/notes", getNotesByUser).Methods("GET")
 	r.HandleFunc("/notes/{id}", getNote).Methods("GET")
 	r.HandleFunc("/notes/{id}", updateNote).Methods("PUT")
 	r.HandleFunc("/notes/{id}", deleteNote).Methods("DELETE")
-	r.HandleFunc("/user", createUser).Methods("POST")
-	r.HandleFunc("/users", getUsers).Methods("GET")
-	r.HandleFunc("/login", loginUser).Methods("POST")
-
-    //r.HandleFunc("/{name}", holaName).Methods("GET")
 
 	log.Print("Corriendo en el puerto 8085")
 	err := http.ListenAndServe(":8085",r)
